e431bff23b77f3e9c67ea888cb18706f25610c77
emrakman55 <akm47emr@gmail.com>
2025-02-07 18:35:42 +0300

    button oldu

--- AnaSayfa.java
+++ AnaSayfa.java
@@ -1,13 +1,25 @@
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class AnaSayfa {
+public class AnaSayfa implements ActionListener {
     JFrame frame = new JFrame("Ana Sayfa");
+    JButton button = new JButton("Profili goster");
 
     public AnaSayfa()
     {
+        button.addActionListener(this);
+        button.setBounds(150,200,200,50);
+        frame.add(button);
+
         frame.setSize(500,500);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setLayout(null);
         frame.setVisible(true);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
     }
 }
