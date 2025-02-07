4325c4823379b89fff15169fafd34c67ca2837c3
emrakman55 <akm47emr@gmail.com>
2025-02-07 18:31:11 +0300

    emir akman

---
+++ AnaProgram.java
@@ -0,0 +1,5 @@
+public class AnaProgram {
+    public static void main(String[] args) {
+        AnaSayfa anaSayfa = new AnaSayfa();
+    }
+}
---
+++ AnaSayfa.java
@@ -0,0 +1,13 @@
+import javax.swing.*;
+
+public class AnaSayfa {
+    JFrame frame = new JFrame("Ana Sayfa");
+
+    public AnaSayfa()
+    {
+        frame.setSize(500,500);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLayout(null);
+        frame.setVisible(true);
+    }
+}
